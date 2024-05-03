32f37fc1531b6bd903145370676d3ad897b5a0d0
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-05-04 04:55:53 +0900

    feat: :zap: moved tab components out

---
+++ components/ControlTab.tsx
@@ -0,0 +1,7 @@
+export default function ControlTab() {
+  return (
+    <div>
+      <p>Tab 1</p>
+    </div>
+  );
+}
---
+++ components/ViewTab.tsx
@@ -0,0 +1,7 @@
+export default function ViewTab() {
+  return (
+    <div>
+      <p>Tab 2</p>
+    </div>
+  );
+}
